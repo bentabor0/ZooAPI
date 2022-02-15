8c58b4c37e2c05ccc348f5cc1075d685aa8b33c5
Ben Tabor <bwtabor@students.ntc.edu>
2022-02-15 10:02:16 -0600

    Added middlewaret to index.js, and added html home page

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import routes from "./src/routes/crmRoutes";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+var path = require('path');
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
@@ -19,14 +21,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
+let indexHTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoo Api</title>
+</head>
+<body>
+    <h1>Welcome to zoo api</h1>
+</body>
+</html>`
 
 routes(app);
 
-app.get('/', (req, res) => 
-    res.send(`ZOO Node and express server running on port ${PORT}`)
-);
+app.get('/', (req, res) => {
+    res.send(indexHTML);
+    console.log('home page hit');
+});
 
 app.listen(PORT, () =>
     console.log(`server is running on port ${PORT}`)
